563296f18707b156e2fd936feaae8206ee150b0b
Kyle McConnaughay <kmcconnaughay@kmcconnaughay-Latitude-E6420.(none)>
2014-02-01 15:27:01 -0500

    Added comments to reader and made a network file to visualize the data

---
+++ network.py
@@ -0,0 +1,12 @@
+# Kyle McConnaughay (2015) & Ryan Louie (2017)
+# Data Science Project with ICF International
+# Franklin W. Olin College of Engineering
+# Spring 2014	
+
+class City:
+	def __init__(self):
+		pass
+		
+
+if __name__ == "__main__":
+	c = City()
--- reader.py
+++ reader.py
@@ -1,18 +1,27 @@
+# Kyle McConnaughay (2015) & Ryan Louie (2017)
+# Data Science Project with ICF International
+# Franklin W. Olin College of Engineering
+# Spring 2014
+
 import csv
 
 class DataReader():
+	"""
+
+	"""
 	def __init__(self, filepath='Data/BKGDAT.txt', delimiter=',', quotechar='"', num_data=10):
 		self.filepath = filepath;
-		self.delimiter = delimiter
-		self.quotechar = quotechar
+		self.delim = delimiter
+		self.qchar = quotechar
 		self.num_data = num_data;
 
+	# Returns a tuple of lists; the first is a list of the headers in a CSV file and the 
 	def getData(self):
 		data = []
 		header = []
 
 		with open(self.filepath, 'rb') as csvfile:
-			reader = csv.reader(csvfile, delimiter=self.delimiter, quotechar=self.quotechar)
+			reader = csv.reader(csvfile, delimiter=self.delim, quotechar=self.qchar)
 			header = reader.next()
 			for i in range(self.num_data):
 				data.append(reader.next())
@@ -20,7 +29,7 @@ class DataReader():
 		return header, data
 
 if __name__ == "__main__":
-	dr = DataReader()
+	dr = DataReader(num_data=20)
 	header, data = dr.getData()
 	print data
 
