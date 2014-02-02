0a265fc36aa91ee867cc8900fdf977bc9b3f9875
themythicaldrago <ryan.louie@students.olin.edu>
2014-02-01 22:30:14 -0500

    Created a flight network that supports directional flight paths (edges) and their associated counts (edge counts)

--- network.py
+++ network.py
@@ -3,14 +3,48 @@
 # Franklin W. Olin College of Engineering
 # Spring 2014	
 
-class Airport:
-	def __init__(self):
-		x = 10
+import pandas as pd 
+import numpy as np 
 
-class Edge:
+
+class Airport:
+	def __init__(self, airport_code):
+		pass
+
+class Edge():
 	def __init__(self):
 		pass
-		
+
+def LoadBookings(n=10000):
+	"""
+	n: number of bookings to load from file
+
+	returns: Bookings Data Frame
+	"""
+	return pd.read_csv("Data/BKGDAT.txt", nrows = n)
+
+def FilterByOriginDestination(df):
+	"""
+	df: bookings, a pandas data frame
+
+	returns: Bookings grouped by Origin and Destination
+	"""
+	return df.groupby(['ORG', 'DES'], sort = False)
+
+def CreateNetwork(df_groupby):
+	"""
+	df_groupby: dataframe groupby object
+
+	returns: a network, in the form of a dictionary, mapping a particular 
+	flight path ('ORG', 'DES') to number of people who traveled on the path. 
+	"""
+	network = {}
+	for flight_path, group in df_groupby:
+		network[flight_path] = len(group)
+	return network
 
 if __name__ == "__main__":
-	pass
+	bookings = FilterByOriginDestination(LoadBookings())
+	flight_network = CreateNetwork(bookings)
+	print flight_network
+
