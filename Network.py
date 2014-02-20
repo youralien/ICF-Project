9e7d60247b8743feb8e7d4a7693d013927d1b57f
themythicaldrago <ryan.louie@students.olin.edu>
2014-02-20 03:41:16 -0500

    Added graphing functionality for looking at overbooking, and calculating cabin load factor

--- Network.py
+++ Network.py
@@ -5,6 +5,7 @@
 
 import pandas as pd 
 import numpy as np 
+import matplotlib.pyplot as plt 
 
 
 class Airport:
@@ -21,7 +22,7 @@ class FeatureFilter():
 		CSV file and groups rows according to various feature values.
 	"""	
 
-	def __init__(self, csvfile='Data/BKGDAT.txt'):
+	def __init__(self, csvfile='Data/BKGDAT_Filtered.txt'):
 		self.csvfile = csvfile
 
 	def loadBookings(self, n):
@@ -100,10 +101,33 @@ class Network():
 			flight = booking_group[0:4]
 			utilization[flight] = float(total_booked[flight]) / total_seats[flight]
 
-		print total_booked[('3/28/2013 0:00:00', 910, 'BEY', 'DXB')]
-		print total_seats[('3/28/2013 0:00:00', 910, 'BEY', 'DXB')]
+		print total_booked[('3/28/2013', 910, 'BEY', 'DXB')]
+		print total_seats[('3/28/2013', 910, 'BEY', 'DXB')]
 		
 		return utilization
+
+	def countOverbookedAndCabinLoadFactor(self):
+		""" Determines which flights overbooking occurs; calculates the 
+		percentage overbooked and the cabin load factor.
+
+		returns: list of tuples (cabin_load_factor, percent_overbooked)
+		"""
+		flights = self.f.filterUniqueFlightsAndBookings(self.entities)
+		
+		ans = []
+
+		for booking_group, data in flights:
+			
+			AUTH = data['AUTH'].mean()
+			CAP = data['CAP'].mean()
+			if AUTH > CAP: # Overbooking occurs when AUTH > CAP
+
+				flight = booking_group[:4]
+				percent_overbooked = float(AUTH - CAP)/CAP
+				cabin_load_factor = float(data['TOTALBKD'].mean())/CAP
+				ans.append((cabin_load_factor, percent_overbooked))
+
+		return ans
 
 	def timeseries(self):
 		flights = self.f.filterUniqueFlights(self.entities)
@@ -119,9 +143,39 @@ class Network():
 		# for flight_path, groupedByPath in flights: #group is a dataframe
 		# 	groupedByDate = groupedByPath.groupby(['DATE'])
 		# 	for date, groupByDate in groupedByDate:
-		# 		print groupByDate
+		# 		print groupByDatek
 
+class Visualizer():
 
+	def __init__(self):
+		pass
+
+	def plotTimeSeries(self, time_series):
+		"""
+		time series is a dictionary of (dates: flight counts)
+		"""
+		sorted_keys = sorted(time_series.keys())
+		print range(len(sorted_keys))
+		print [time_series[date] for date in sorted_keys]
+
+		fig = plt.figure()
+		ax = fig.add_subplot(1, 1, 1)
+		ax.plot(range(len(sorted_keys)), [time_series[date] for date in sorted_keys])
+		ax.xaxis.set_ticklabels(sorted_keys)
+		for tick in ax.xaxis.get_major_ticks():
+			tick.label.set_fontsize(8)
+			tick.label.set_rotation('vertical')
+		plt.show()
+
+	def date2DayOfWeek(self, date):
+		"""
+		date : string 'm/d/yyyy' or 'mm/dd/yyyy'
+		"""
+		month, day, year = date.split('/')
+		month, day, year = int(month), int(day), int(year)
+
+		day = datetime.date(year, month, day) 
+		return day.strftime("%A")
 
 def main():
 	num_records = 'all'
@@ -131,15 +185,33 @@ def main():
 	# 	print flight, utilization
 	# print n.entities.loc[:, 'TOTALBKD']
 	x = n.timeseries()
+
 	# x = n.countMeanUtilization()
 	# for key, value in x.items():
 	# 	print key, value
 
-
-if __name__ == "__main__":
+def timeVsFlights():
 	num_records = 'all'
 	n = Network(num_records)
 	x = n.timeseries()
+	v = Visualizer()
+	keys = x.keys()
+	for i, key in enumerate(keys):
+		print key, len(x[key])
+	v.plotTimeSeries(x[keys[0]])
+
+def overbookingVsCabinLoadFactor():
+	num_records = 'all'
+	n = Network(num_records)
+	utilization = n.countOverbookedAndCabinLoadFactor()
+	plt.plot(utilization)
+	plt.xlabel('Cabin Load Factor (units?)')
+	plt.ylabel('Overbooking (units?)')
+	plt.show()
+
+
+if __name__ == "__main__":
+	overbookingVsCabinLoadFactor()
 
 
 
