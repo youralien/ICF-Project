4497a008970f54959c4f33e5d290a97691bdd28c
themythicaldrago <ryan.louie@students.olin.edu>
2014-02-21 16:51:37 -0500

    Here's to kyle for mergin

--- Main.py
+++ Main.py
@@ -4,7 +4,8 @@ from Utils import Utils
 from FeatureFilter import FeatureFilter
 
 def main():
-	pass
+	n = Network(1000)
+	print n.countCabinCapacityPerFlight()	
 
 if __name__ == '__main__':
 	main()
