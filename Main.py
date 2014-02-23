c2621919e6cf102537b2c13280415aa4a381661d
Kyle McConnaughay <kmcconnaughay@kmcconnaughay-Latitude-E6420.(none)>
2014-02-23 13:04:32 -0500

    Fixed Main

--- Main.py
+++ Main.py
@@ -1,21 +1,3 @@
-
-		break
-		print list(d.sort(columns='KEYDAY', ascending=False)['KEYDAY'])
-		plt.show()
-		plt.plot(KEYDAY, BKD)
-
-		BKD = list(d.sort(columns='KEYDAY', ascending=False)['BKD'])
-		KEYDAY = list(-d.sort(columns='KEYDAY', ascending=False)['KEYDAY'])
-	for g, d in fltbk:
-	fltbk = n.f.getUniqueFlightsAndBookings()
-	n = Network(1000)
-if __name__ == '__main__':
-
-	print type(orgdes)	
-	orgdes = n.f.getUniqueOrgDes()
-	n = Network(1000)
-def main():
-
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -23,3 +5,24 @@ from FeatureFilter import FeatureFilter
 from Utils import Utils
 from Visualizer import Visualizer
 from Network import Network
+
+def main():
+	n = Network(1000)
+	fltbk = n.f.getUniqueFlightsAndBookings()
+	
+	for g, d in fltbk:
+		print list(d.sort(columns='KEYDAY', ascending=False)['KEYDAY'])
+		BKD = list(d.sort(columns='KEYDAY', ascending=False)['BKD'])
+		KEYDAY = list(-d.sort(columns='KEYDAY', ascending=False)['KEYDAY'])
+
+		plt.plot(KEYDAY, BKD)
+		plt.show()
+		break
+
+if __name__ == '__main__':
+	main()	
+	orgdes = n.f.getUniqueOrgDes()
+	print type(orgdes)
+	n = Network(1000)
+
+
