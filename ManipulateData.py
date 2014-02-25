b25fb1077c5d5bf3694f98d6e8a2018bc400d66b
Kyle McConnaughay <kmcconnaughay@kmcconnaughay-Latitude-E6420.(none)>
2014-02-25 15:37:30 -0500

    Normalized the data

---
+++ ManipulateData.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from pandas.util.testing import assert_series_equal
+
+def RemoveHourMinuteSecond(oldfilename, newfilename):
+
+	oldfile = open(oldfilename, 'r')
+	newfile = open(newfilename, 'w')
+
+	for oldline in oldfile:
+		newline = oldline.replace(' 0:00:00', '')
+		newfile.write(newline)
+
+	oldfile.close()
+	newfile.close()
+
+def NormalizeData(oldfilename, newfilename):
+	df = pd.read_csv(oldfilename, nrows=100)
+	
+	old_row = df.loc[1, ['DATE', 'FLT', 'ORG', 'DES', 'BC']]
+	old_index = 0
+	max_keyday = -1
+
+	for i in range(len(df)):
+		capacity = df.loc[i, 'CAP']
+		df.loc[i, 'BKD'] = float(df.loc[i, 'BKD']) / capacity
+		df.loc[i, 'AUTH'] = float(df.loc[i, 'AUTH']) / capacity
+		df.loc[i, 'TOTALBKD'] = float(df.loc[i, 'TOTALBKD']) / capacity
+		df.loc[i, 'AVAIL'] = float(df.loc[i, 'AVAIL']) / capacity
+
+		max_keyday = max(max_keyday, df.loc[i, 'KEYDAY'])
+		
+		if not (old_row == df.loc[i, ['DATE', 'FLT', 'ORG', 'DES', 'BC']]).all():
+			for j in range(old_index, i):
+				df.loc[j, 'KEYDAY'] = float(df.loc[j, 'KEYDAY']) / max_keyday
+
+			old_index = i
+			max_keyday = df.loc[i, 'KEYDAY']
+			old_row = df.loc[i, ['DATE', 'FLT', 'ORG', 'DES', 'BC']]
+
+	df.to_csv(newfilename, index=False)
+
+
+if __name__ == "__main__":
+	# RemoveHourMinuteSecond('Data/BKGDAT.txt', 'Data/BKGDAT_Filtered.txt')
+	NormalizeData('Data/BKGDAT_Filtered.txt', 'Data/Normalized_BKGDAT_Filtered.txt')
