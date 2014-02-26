edd65c0ab2aba9abf326f6db9bc17c01d2f16565
themythicaldrago <ryan.louie@students.olin.edu>
2014-02-25 20:13:41 -0500

    Wrote a main function that attempts to produce a CDF of cabin load factor. Cdf produced are buggy for num_records > 100050.

--- Network.py
+++ Network.py
@@ -129,20 +129,23 @@ class Network():
 		return time_series
 
 def main():
-	num_records = 'all'
-	n = Network(num_records)
-
-if __name__ == '__main__':
-	#main()
 	import thinkplot
 	import thinkstats2
 
+	# nrows to be loaded from data file. 'all' loads the whole file.
 	num_records = 100500
+
 	n = Network(num_records)
+	
 	cabin_load_factor_list =  n.countFinalCabinLoadFactor().values()
+
 	print "cabin_load_factor_list: \n", cabin_load_factor_list
 	print "length of clf_list: ", len(cabin_load_factor_list)
 	print "maximum cabin load factor: ", max(cabin_load_factor_list)
+	
 	cabin_load_factor_cdf = thinkstats2.MakeCdfFromList(cabin_load_factor_list)
 	thinkplot.Cdf(cabin_load_factor_cdf)
 	thinkplot.show()
+
+if __name__ == '__main__':
+	main()
