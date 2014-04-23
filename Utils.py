7ff58745003fe282bcc68bbe0e045513f21988c6
Kyle <kmcconnaughay@kmcconnaughay-Latitude-E6420.(none)>
2014-04-23 12:36:42 -0400

    Added a compare function for BCs that can be used to sort the booking classes

--- Utils.py
+++ Utils.py
@@ -39,6 +39,13 @@ class Utils():
                     (5,'P','J'),
                     (6,'R','J')]
 
+    @staticmethod
+    def compareBCs(bc1, bc2):
+        c1, r1 = Utils.mapBookingClassToCabinHierarchy(bc1)
+        c2, r2 = Utils.mapBookingClassToCabinHierarchy(bc2)
+
+        return r2 - r1
+
 
     @staticmethod
     def mapBookingClassToCabinHierarchy(booking_class):
